cb11672e01c2dfc40a9832e0804498e9313460b4
Joshua <fhbzyc@gmail.com>
2015-01-07 21:07:34 +0800

    update

--- protocol/request.go
+++ protocol/request.go
@@ -5,7 +5,6 @@ import "encoding/json"
 type Request struct {
 	//	Jsonrpc string        `json:"jsonrpc"`
 	Id      string        `json:"id"`
-	Service string        `json:"service"`
 	Method  string        `json:"method"`
 	Params  []interface{} `json:"params"`
 }
